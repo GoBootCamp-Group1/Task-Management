7fd67b9c32251ee865fc219a50cab166465287a0
Saman <samankarbasi@live.com>
2024-07-08 17:42:06 +0330

    -utils method for get user id within fiber context

--- pkg/utils/utils.go
+++ pkg/utils/utils.go
@@ -3,10 +3,20 @@ package utils
 import (
 	"crypto/sha256"
 	"encoding/hex"
+	"github.com/GoBootCamp-Group1/Task-Management/pkg/jwt"
+	"github.com/gofiber/fiber/v2"
 )
 
 func HashPassword(password string) string {
 	h := sha256.New()
 	h.Write([]byte(password))
 	return hex.EncodeToString(h.Sum(nil))
+}
+
+func GetUserID(c *fiber.Ctx) (uint, error) {
+	userClaims, ok := c.Locals(jwt.UserClaimKey).(*jwt.UserClaims)
+	if !ok {
+		return 0, fiber.NewError(fiber.StatusUnauthorized, "Invalid user claims")
+	}
+	return userClaims.UserID, nil
 }
