c7ede34b2aaff38a9c3e528e7ed02ec7adf98c29
mrkingali <alimardalishahi8013@gmail.com>
2024-07-10 14:10:11 +0330

    add weight to role and change func to return role instead of string

--- internal/adapters/storage/entities/role.go
+++ internal/adapters/storage/entities/role.go
@@ -8,4 +8,5 @@ type Role struct {
 	gorm.Model
 	Name        string `gorm:"type:varchar(255);not null;unique"`
 	Description string `gorm:"type:text"`
+	Weight      int    `gorm:"type:int"`
 }
--- internal/adapters/storage/mappers/role.go
+++ internal/adapters/storage/mappers/role.go
@@ -2,37 +2,39 @@ package mappers
 
 import (
 	"github.com/GoBootCamp-Group1/Task-Management/internal/adapters/storage/entities"
-	"github.com/GoBootCamp-Group1/Task-Management/internal/core/domain"
+	"github.com/GoBootCamp-Group1/Task-Management/internal/core/domains"
 	"github.com/GoBootCamp-Group1/Task-Management/pkg/fp"
 	"gorm.io/gorm"
 )
 
-func DomainToRoleEntity(role *domain.Role) *entities.Role {
+func DomainToRoleEntity(role *domains.Role) *entities.Role {
 	return &entities.Role{
 		Model: gorm.Model{
 			ID: role.ID,
 		},
 		Name:        role.Name,
 		Description: role.Description,
+		Weight:      role.Weight,
 	}
 }
 
-func RoleEntityToDomain(entity *entities.Role) *domain.Role {
-	return &domain.Role{
+func RoleEntityToDomain(entity *entities.Role) *domains.Role {
+	return &domains.Role{
 		ID:          entity.ID,
 		Name:        entity.Name,
 		Description: entity.Description,
+		Weight:      entity.Weight,
 	}
 }
 
-func RoleEntitiesToDomain(roleEntities []entities.Role) []domain.Role {
-	return fp.Map(roleEntities, func(entity entities.Role) domain.Role {
+func RoleEntitiesToDomain(roleEntities []entities.Role) []domains.Role {
+	return fp.Map(roleEntities, func(entity entities.Role) domains.Role {
 		return *RoleEntityToDomain(&entity)
 	})
 }
 
-func RoleDomainsToEntity(roleDomains []domain.Role) []entities.Role {
-	return fp.Map(roleDomains, func(role domain.Role) entities.Role {
+func RoleDomainsToEntity(roleDomains []domains.Role) []entities.Role {
+	return fp.Map(roleDomains, func(role domains.Role) entities.Role {
 		return *DomainToRoleEntity(&role)
 	})
 }
