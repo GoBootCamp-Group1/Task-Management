925931407ee139c55fba82bdb716453f6991fb07
mrkingali <alimardalishahi8013@gmail.com>
2024-07-11 19:30:10 +0330

    write initial code to create basic role

--- api/http/setup.go
+++ api/http/setup.go
@@ -1,7 +1,9 @@
 package http
 
 import (
+	"context"
 	"fmt"
+	"github.com/GoBootCamp-Group1/Task-Management/internal/core/domains"
 
 	"github.com/GoBootCamp-Group1/Task-Management/api/http/middlerwares"
 	"github.com/GoBootCamp-Group1/Task-Management/api/http/routes"
@@ -33,8 +35,48 @@ func Run(cfg config.Server, app *app.Container) {
 		log.ErrorLog.Fatal(err)
 	}
 	log.InfoLog.Println("Starting the application...")
+
+	//add basic role to db
+	createRolesInDb(app)
 }
 
 func userRoleChecker() fiber.Handler {
 	return middlerwares.RoleChecker("user")
+}
+
+func createRolesInDb(cotainer *app.Container) {
+
+	maintainerRoleEnum, _ := domains.ParseRole("Maintainer")
+	editorRoleEnum, _ := domains.ParseRole("Editor")
+	ownerRoleEnum, _ := domains.ParseRole("Owner")
+	viewerRoleEnum, _ := domains.ParseRole("Viewer")
+	maintainerRole := domains.Role{
+		ID:          0,
+		Name:        maintainerRoleEnum.String(),
+		Description: "its maintainer role",
+		Weight:      int(maintainerRoleEnum),
+	}
+	editorRole := domains.Role{
+		ID:          0,
+		Name:        editorRoleEnum.String(),
+		Description: "its maintainer role",
+		Weight:      int(editorRoleEnum),
+	}
+	ownerRole := domains.Role{
+		ID:          0,
+		Name:        ownerRoleEnum.String(),
+		Description: "its maintainer role",
+		Weight:      int(ownerRoleEnum),
+	}
+	viewerRole := domains.Role{
+		ID:          0,
+		Name:        viewerRoleEnum.String(),
+		Description: "its maintainer role",
+		Weight:      int(viewerRoleEnum),
+	}
+	//unhandled error
+	_ = cotainer.RoleService().CreateRole(context.Background(), &maintainerRole)
+	_ = cotainer.RoleService().CreateRole(context.Background(), &editorRole)
+	_ = cotainer.RoleService().CreateRole(context.Background(), &ownerRole)
+	_ = cotainer.RoleService().CreateRole(context.Background(), &viewerRole)
 }
